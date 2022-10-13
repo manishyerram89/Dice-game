ff81d90803792bb2cf5c2094a7fe2445b03f8d67
manishyerram89 <101630848+manishyerram89@users.noreply.github.com>
2022-10-13 14:02:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomnumber1 = Math.floor(Math.random() * 6) + 1;
+var wholenumber ="images/dice"+ randomnumber1+".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",wholenumber);
+
+var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+var wholenumber ="images/dice"+ randomnumber2+".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",wholenumber);
+
+if (randomnumber1 > randomnumber2){
+  document.querySelector("h1").innerHTML="Player 1 wins !";
+}
+else if(randomnumber2 > randomnumber1){
+  document.querySelector("h1").innerHTML="Player 2 wins !";
+}
+else if(randomnumber1 == randomnumber2){
+  document.querySelector("h1").innerHTML="Draw";
+}
